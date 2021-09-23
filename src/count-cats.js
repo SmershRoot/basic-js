ac15f3f130628edbd116dde4c0713258e93fa955
Щербаков Александр <Smersh.Root@gmail.com>
2021-09-23 13:29:57 +0500

    Update count-cats.js

--- src/count-cats.js
+++ src/count-cats.js
@@ -18,7 +18,7 @@ export default function countCats(matrix) {
   let result = 0;
 	for(let i = 0; i < matrix.length; i++){
 		for(let j = 0; j < matrix[i].length; j++){
-			if(matrix[i,j]=='^^'){
+			if(matrix[i][j]/*.replace(/\s/g, '')*/=='^^'){
 				result++;
 			}
 		}
